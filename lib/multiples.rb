6d196e9b93a03f30de4d0f6805d1ae8bfdd085a7
jessicashannon <jessdahling@gmail.com>
2015-10-12 10:49:01 -0400

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+
+# - Find the __sum__ of all the multiples of 3 or 5 below 1000.
+
+# Declare sum variable equal to 0
+# Iterate over each number between 1 and 999
+# Check if each is divisible by 3 or 5
+# If so, add it to sum
+# When done, return sum
+
+def collect_multiples(limit)
+  array = []
+  (1...limit).each do |num|
+    array << num if (num % 3 == 0) || (num % 5 == 0)
+  end
+  array
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    array = []
+    (1...@limit).each do |num|
+      array << num if (num % 3 == 0) || (num % 5 == 0)
+    end
+    array
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+
+end
